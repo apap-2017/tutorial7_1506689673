b2755e0e09175c5e6f3f7f0109918ac5e6c5c2e0
fizhazuharah <fizhafizha@gmail.com>
2017-11-04 04:06:18 +0700

    add tutorial07producer

---
+++ Tutorial07Producer/src/main/java/com/example/controller/CourseController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.CourseModel;
+import com.example.service.CourseService;
+
+@Controller
+public class CourseController {
+	@Autowired
+	private CourseService courseDAO;
+	
+	@RequestMapping("/course/view/{id}")
+	public String viewPath(Model model, @PathVariable(value="id") String id_course) {
+		CourseModel course = courseDAO.selectCourse(id_course);
+
+		if (course != null) {
+			model.addAttribute("course", course);
+			return "view-course";
+		} else {
+			model.addAttribute("id_course", id_course);
+			return "not-found-course";
+		}
+	}
+}
